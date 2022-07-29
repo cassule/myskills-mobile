ab4705264b005ded3ffdfa4d163ad099b7f42a86
Josué Francisco <josuefrancisco2017@outlook.pt>
2022-07-29 01:42:31 +0100

    intitial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  FlatList,
+} from "react-native";
+
+import { Button } from "../components/Button";
+import { SkillCard } from "../components/SkillCard";
+
+interface skillData {
+  id: string;
+  name: string;
+}
+
+export function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState<skillData[]>([]);
+  const [greeting, setGreeting] = useState('');
+
+  function handleAddNewSkill() {
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill
+    }
+
+    setMySkills([...mySkills, data]);
+  }
+
+  function handleRemoveSkill(id: string) {
+    setMySkills(oldState => oldState.filter(
+      skill => skill.id !== id
+    ));
+  }
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      setGreeting('Good morning');
+    }
+    else if (currentHour >= 12 && currentHour <= 18) {
+      setGreeting('Good afternoon');
+    } else {
+      setGreeting('Good night');
+    }
+
+  }, [])
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Welcome, Josué
+      </Text>
+
+      <Text style={styles.greetings}>
+        {greeting}
+      </Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
+        onChangeText={setNewSkill}
+      />
+
+      <Button
+        title="Add"
+        onPress={handleAddNewSkill}
+      />
+
+      <Text style={[styles.title, { marginVertical: 50 }]}>
+        My Skills
+      </Text>
+
+      <FlatList
+        data={mySkills}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <SkillCard
+            title={item.name}
+            onPress={() => handleRemoveSkill(item.id)}
+          />
+        )}
+      />
+
+
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingVertical: 70,
+    paddingHorizontal: 30
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+  greetings: {
+    color: '#fff',
+  }
+})
